6129a48f19c81291dfad44919158c0eaa7cdd905
jasonjohnson1 <johnsonj1995@yahoo.com>
2015-03-04 18:22:47 -0600

    Update Driver.java
    add javadoc

--- Project 3/src/src/Driver.java
+++ Project 3/src/src/Driver.java
@@ -1,6 +1,22 @@
+/**
+ * Project #3
+ * CS 2334, Section 010
+ * March 4, 2015
+ * <P>
+ * A user searches through a database of people, places,
+ * and teams with his or her preferred criteria and
+ * data is displayed to the user about that search term.
+ * </P>
+ * @version 1.0
+ */
 public class Driver 
 {
 
+	/**
+	 * User inputs his search criteria and information
+	 * is displayed based on that criteria.
+	 * @param args
+	 */
 	public static void main(String[] args) 
 	{
 
